972e4b7f95f0f3c12ebcbf036f2d565e54d2c2d0
Rob Clark <rob@ti.com>
2010-03-13 12:51:01 -0600

    updates for L24.4-p2
    note: start refactoring common parts of build script into common file
    used in omap4-omx and omap4-gstreamer

--- build-it-all.sh
+++ build-it-all.sh
@@ -6,6 +6,9 @@ if [ `uname -m` != "arm" ]; then
 	exec sb2 $0 $*
 fi
 
+cd `dirname $0`
+dir=`pwd`
+
 # setup some env vars for build:
 export TARGET=${TARGET:-`pwd`/target}
 export NOCONFIGURE=1
@@ -60,119 +63,7 @@ components="\
 "
 # todo.. add gst-plugin-bc if dependencies are satisfied..
 
-###############################################################################
-# Helper functions:
-
-build_subdir="00BUILD"
-force_bootstrap="no"
-do_it=build_it
-extra_configure_args=""
-
-function build_it() {
-	component=$1
-	shift 1
-	args=$*
-	echo ""
-	echo ""
-	echo "############################################################"
-	echo "###### Building $component"
-
-	bootstrap=""
-	for f in autogen.sh bootstrap.sh bootstrap; do
-		if [ -x "$component/$f" ]; then
-			bootstrap=$f
-			break;
-		fi
-	done
-
-	if [ ! -n $bootstrap ] && [ ! -x "$component/configure" ]; then
-		echo "I am confused!"
-		return 1
-	fi
-
-	build_dir="$component/$build_subdir"
-
-	if [ $force_bootstrap = "yes" ] || [ ! -e $build_dir/Makefile ]; then
-		if [ -n "$bootstrap" ]; then
-			echo ""
-			echo "### bootstrap $component"
-			(cd $component; ./$bootstrap) || return 1
-			# some bootstrap files are silly and don't let you supress 
-			# the configure step:
-			if [ -e $component/Makefile ]; then
-				(cd $component; make distclean)
-			fi
-		fi
-	
-		echo ""
-		echo "### configure"
-		mkdir -p $build_dir
-		(cd $build_dir; ../configure $args $extra_configure_args) || return 1
-		if [ -r $build_dir/config.h ]; then
-			sed s/"$escaped_target"//g $build_dir/config.h > $build_dir/.tmp.config.h &&
-				mv $build_dir/.tmp.config.h $build_dir/config.h
-			cat $build_dir/config.h
-		fi
-	fi
-
-	echo ""
-	echo "### make $component"
-	(cd $build_dir; make -w -j4) || return 1
-
-	echo ""
-	echo "### install $component"
-	(cd $build_dir; make install) || return 1
-}
-
-function clean_it() {
-	component=$1
-	echo ""
-	echo ""
-	echo "############################################################"
-	echo "####### Cleaning $component"
-	
-	build_dir="$component/$build_subdir"
-	
-	if [ -e $build_dir/Makefile ]; then
-		(cd $build_dir; make distclean) || echo "error cleaning $component... continuing"
-	fi
-}
-
-yes_all="false"
-function prompt_yes_no() {
-	if [ $yes_all = "true" ]; then
-		echo "  Y/N/A> a"
-		return 0
-	fi
-	while `true`; do
-		echo -n "  Y/N/A> "
-		read -n 1 result
-		echo ""
-		case $result in
-			A|a) yes_all="true"; return 0 ;;
-			Y|y) return 0 ;;
-			N|n) return 1 ;;
-			*) echo "invalid input: $result"
-		esac
-	done
-}
-
-
-function check_update_submodule_status() {
-	dir=$1
-	pushd . > /dev/null
-	cd $dir
-	git submodule init
-	for submodule in `git submodule status | grep "^[-+]" | awk '{print $2}'`; do 
-		echo "Ok to update $dir/$submodule (which will overwrite any private changes you have made)?"
-		prompt_yes_no && force_bootstrap="yes" && git submodule update $submodule
-	done
-	popd > /dev/null
-}
-
-check_update_submodule_status . || exit $?
-check_update_submodule_status omap4-omx || exit $?
-yes_all="false"   # reset in case someone adds other calls to prompt_yes_no()
+source $dir/omap4-omx/common-build-utils.sh
 
 ###############################################################################
 # Argument parsing:
@@ -204,19 +95,14 @@ for arg in $*; do
 	esac
 done
 
-###############################################################################
-# Main loop:
+yes_all="false"   # reset yes/no/all to != all..
+check_update_submodule_status . || exit $?
+check_update_submodule_status omap4-omx || exit $?
+yes_all="false"   # reset in case someone adds other calls to prompt_yes_no()
 
-SAVED_IFS=$IFS
-IFS="
-"
-for line in $components; do
-	IFS=$SAVED_IFS
-	$do_it $line
-	if [ $? != 0 ]; then
-		echo "failed on $component, bailing out"
-		exit 1
-	fi
-done
-IFS=$SAVED_IFS
+# workaround for tiler build:
+mkdir -p omap4-omx/tiler/memmgr/m4
+mkdir -p omap4-omx/tiler/d2c/m4
+
+main_loop
 
